921fd214be7a155a881cd67ee1274c03a20df546
alberto <albon_marvel@hotmail.com>
2022-09-04 01:05:57 -0500

    add some test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { BowAndArrowBehavior } from './patterns/strategy/classes/BowAndArrowBehavior/bow-and-arrow-behavior';
+import { King } from './patterns/strategy/classes/King/king';
+import { Queen } from './patterns/strategy/classes/Queen/queen';
+import { SwordBehavior } from './patterns/strategy/classes/SwordBehavior/sword-behavior';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -28,4 +32,24 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('patternsCourse app is running!');
   });
+
+
+  fit('should create King with Sword', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.createCharacter();
+    expect(app.character instanceof King).toBeTruthy();
+  });
+
+  fit('should create Queen with Bow and Arrow', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.characterStr = 'queen';
+    app.weaponStr = 'bowAndArrow'
+    app.createCharacter();
+    expect(app.character instanceof Queen).toBeTruthy();
+    expect(app.weapon instanceof BowAndArrowBehavior).toBeTruthy();
+  });
+
+
 });
